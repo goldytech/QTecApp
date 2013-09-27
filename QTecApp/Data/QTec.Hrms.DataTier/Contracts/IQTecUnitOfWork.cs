cd6b5cee2d4614dedf0083300c0b73628077ddc8
goldytech <afzal.qureshi@outlook.com>
2013-09-27 16:49:12 +0530

    Completed Database Tier Implementation

---
+++ QTecApp/Data/QTec.Hrms.DataTier/Contracts/IQTecUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿namespace QTec.Hrms.DataTier.Contracts
+{
+    using QTec.Hrms.Models;
+
+    /// <summary>
+    /// The interface of QTEC Unit of Work
+    /// </summary>
+    public interface IQTecUnitOfWork
+    {
+        /// <summary>
+        /// Gets the employee repository.
+        /// </summary>
+        /// <value>The employee repository.</value>
+        IEmployeeRepository EmployeeRepository
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the designation repository.
+        /// </summary>
+        /// <value>The designation repository.</value>
+        IRepository<Designation> DesignationRepository
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Commits the changes in database.
+        /// </summary>
+        void Commit();
+    }
+}
---
+++ QTecApp/Data/QTec.Hrms.DataTier/QTecUnitOfWork.cs
@@ -0,0 +1,94 @@
+﻿  
+namespace QTec.Hrms.DataTier
+{
+    using System;
+    using QTec.Hrms.DataTier.Contracts;
+    using QTec.Hrms.Models;
+
+    /// <summary>
+    /// The QTEC unit of work.
+    /// </summary>
+    public class QTecUnitOfWork : IQTecUnitOfWork, IDisposable
+    {
+        /// <summary>
+        /// The repository provider.
+        /// </summary>
+        private readonly IRepositoryProvider repositoryProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QTecUnitOfWork" /> class.
+        /// </summary>
+        /// <param name="repositoryProvider">The repository provider.</param>
+        public QTecUnitOfWork(IRepositoryProvider repositoryProvider)
+        {
+            this.DbContext = new QTecDataContext();
+            this.repositoryProvider = repositoryProvider;
+            this.repositoryProvider.DbContext = this.DbContext;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing,
+        /// or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the employee repository.
+        /// </summary>
+        /// <value>The employee repository.</value>
+        public IEmployeeRepository EmployeeRepository
+        {
+            get
+            {
+                return this.GetRepo<IEmployeeRepository>();
+            }
+        }
+
+        /// <summary>
+        /// Gets the designation repository.
+        /// </summary>
+        /// <value>The designation repository.</value>
+        public IRepository<Designation> DesignationRepository
+        {
+            get
+            {
+                return this.GetStandardRepo<Designation>();
+            }
+        }
+
+        /// <summary>
+        /// Commits the changes in database.
+        /// </summary>
+        public void Commit()
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+
+        protected IRepositoryProvider RepositoryProvider
+        {
+            get;
+            set;
+        }
+
+        private IRepository<T> GetStandardRepo<T>() where T : class
+        {
+            return RepositoryProvider.GetRepositoryForEntityType<T>();
+        }
+
+        private T GetRepo<T>() where T : class
+        {
+            return RepositoryProvider.GetRepository<T>();
+        }
+
+        private QTecDataContext DbContext
+        {
+            get;
+            set;
+        }
+    }
+}
