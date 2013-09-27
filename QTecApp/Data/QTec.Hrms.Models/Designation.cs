86b830724f686df8f7115665a20e0c8f8b11f53e
goldytech <afzal.qureshi@outlook.com>
2013-09-27 12:33:54 +0530

    Created Models

---
+++ QTecApp/Data/QTec.Hrms.Models/Designation.cs
@@ -0,0 +1,36 @@
+﻿#region License QTec
+////---------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="Designation.cs" company="QTec">
+//      License under GPL
+// </copyright>
+// <summary>
+//      Represents a class for Designation.
+// </summary>
+////---------------------------------------------------------------------------------------------------------------------------------------------------------
+#endregion
+
+namespace QTec.Hrms.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The designation.
+    /// </summary>
+    public class Designation
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the employees.
+        /// </summary>
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
