1ef420e6affa5b2aeb1127dc5b4386f930bff773
goldytech <afzal.qureshi@outlook.com>
2013-09-27 10:17:58 +0530

    Created Initial Project Structure
    Added the different layers in the solution

---
+++ QTecApp/Data/QTec.Hrms.DataTier/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTec.Hrms.DataTier
+{
+    public class Class1
+    {
+    }
+}
